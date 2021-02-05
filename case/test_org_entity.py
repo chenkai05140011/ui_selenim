de8310f16c39ab296b1dcdeed7c2261b6f450eb6
chenkai <1655267850@qq.com>
2021-02-05 17:29:23 +0800

    增加校验

--- case/test_org_entity.py
+++ case/test_org_entity.py
@@ -3,34 +3,34 @@ import allure
 from pages.tag import Tag
 from pages.org_entity_page import Org_Entity
 
-@allure.epic("行政执法项目")
-@allure.feature("打标签")
-class Test_Tag():
+# @allure.epic("行政执法项目")
+# @allure.feature("打标签")
+# class Test_Tag():
+#
+#     @allure.title("机构类打标签")
+#     @allure.testcase("http://jira.icinfo.co/browse/ZJXZZF-349#")
+#     @allure.issue("http://jira.icinfo.co/browse/ZJXZZF-315")
+#     @allure.description("case描述：新增tag")
+#     @allure.severity("blocker")
+#     def test_add_tag(self, login_cf):
+#         '''
+#         :param login_cf: 登录
+#         1.新增tag
+#         :return:
+#         '''
+#         # driver = login_cf
+#         # web = Tag(driver)
+#         # web.click_tag()
+#         # text = web.add_tag()
+#         # assert text == "标签添加成功"
+#         pass
 
-    @allure.title("机构类打标签")
-    @allure.testcase("http://jira.icinfo.co/browse/ZJXZZF-349#")
-    @allure.issue("http://jira.icinfo.co/browse/ZJXZZF-315")
-    @allure.description("case描述：新增tag")
-    @allure.severity("blocker")
-    def test_add_tag(self, login_cf):
-        '''
-        :param login_cf: 登录
-        1.新增tag
-        :return:
-        '''
-        # driver = login_cf
-        # web = Tag(driver)
-        # web.click_tag()
-        # text = web.add_tag()
-        # assert text == "标签添加成功"
-        pass
-
-    def test_org_tag(self, login_cf):
-        driver = login_cf
-        web = Org_Entity(driver)
-        web.menu_org()
-        text = web.add_org()
-        assert text == "批量加入管辖成功"
+    # def test_org_tag(self, login_cf):
+    #     driver = login_cf
+    #     web = Org_Entity(driver)
+    #     web.menu_org()
+    #     text = web.add_org()
+    #     assert text == "批量加入管辖成功"
 
 
 
--- pages/org_entity_page.py
+++ pages/org_entity_page.py
@@ -2,28 +2,55 @@ from common.base_page import BasePage
 
 class Org_Entity(BasePage):
     # 登入
-    loc_1 = ("css", "span[title='监管智库']")
-    loc_2 = ("css", "span[title='监管对象']")
-    loc_3 = ("css", "span[title='机构类主体']")
-    loc_4 = ("css", ".ant-table-thead [type='checkbox']")
-    loc_5 = ("xpath", "//button[@nz-wave='[object Object]']")
-    loc_6 = ("xpath", "//span[text()='公安禁毒管理']")
-    loc_7 = ("xpath", "(//input[@placeholder='请输入'])[1]")
-    loc_8 = ("xpath", "//input[@type='checkbox']")
-    loc_9 = ("xpath", "(//div[@unselectable='unselectable'])[1]")
-    loc_10 = ("xpath", "(//li[@unselectable='unselectable'])[1]")
-    loc_11 = ("xpath", "(//div[@class='ant-select-selection__rendered'])[2]")
-    loc_12 = ("xpath", "(//input[@placeholder='请输入'])[2]")
-    loc_13 = ("name", "textarea")
+    loc_menu = ("css", "span[title='监管智库']")
+    loc_menu_supervision = ("css", "span[title='监管对象']")
+    loc_menu_supervision_org = ("css", "span[title='机构类主体']")
 # 缺少自动标签部分
-    loc_14 = ("xpath", "(//button[@nz-wave='[object Object]'])[2]")
-    loc_17 = ("css", ".ant-message span")
-    loc_15 = ("xpath", "//span[text()='陈凯一直自动标签 (1级) ']")
-    loc_16 = ("xpath", "(//button[@nz-wave='[object Object]'])[3]")
 
-    def org_tag(self):
-        self.click(self.loc_1)
-        self.click(self.loc_2)
-        self.click(self.loc_3)
-        self.click(self.loc_4)
+    def menu_org(self):
+        self.click(self.loc_menu)
+        self.click(self.loc_menu_supervision)
+        self.click(self.loc_menu_supervision_org)
         self.sleep(5)
+
+
+
+    def search_org(self):
+        pass
+
+    loc_add_org_button = ("css", ".operate .ant-btn-primary")
+    loc_add_org_name = ("css", ".ant-modal-content [formcontrolname=orgEntityName]")
+    loc_add_org_status = ("css", "[formcontrolname=avaliable]")
+    loc_add_org_status_all = ("css", "[role=menu] li:nth-child(1)")
+    loc_add_org_status_n = ("css", "[role=menu] li:nth-child(2)")
+    loc_add_org_status_y = ("css", "[role=menu] li:nth-child(3)")
+    loc_add_org_search = ("css", ".ant-modal-body .search-area .ant-btn-primary")
+    loc_tick = ("css", ".ant-modal-body [type='checkbox']")
+    loc_add_org_manage_button = ("css", ".ant-modal-body>div button")
+    loc_alert = ("css", ".ant-message span")
+    def add_org(self, name='台州市跃驰汽配有限公司'):
+        self.click(self.loc_add_org_button)
+        self.type(self.loc_add_org_name, name)
+        self.click(self.loc_add_org_status)
+        self.click(self.loc_add_org_status_y)
+        self.click(self.loc_add_org_search)
+        self.click(self.loc_tick)
+        self.click(self.loc_add_org_manage_button)
+        self.get_img()
+        text = self.get_text(self.loc_alert)
+        return text
+
+
+    def manage_org(self):
+        pass
+
+    def batch_add_tag_org(self):
+        pass
+
+    def batch_del_manage(self):
+        pass
+
+    def batch_del_tag(self):
+        pass
+
+
